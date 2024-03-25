bfd0816065e437a1f745910c3e80fb925c7e2f55
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-03-25 11:47:29 +0100

    pages vue

--- assets/pages/AllCardsPage.vue
+++ assets/pages/AllCardsPage.vue
@@ -1,34 +1,102 @@
-<script setup>
-import { onMounted, ref } from 'vue';
-import { fetchAllCards } from '../services/cardService';
+<script>
+import { fetchAllCardsWithPagination, fetchCardsBySetCode, fetchSetCode } from '../services/cardService';
 
-const cards = ref([]);
-const loadingCards = ref(true);
-
-async function loadCards() {
-    loadingCards.value = true;
-    cards.value = await fetchAllCards();
-    loadingCards.value = false;
-}
-
-onMounted(() => {
-    loadCards();
-});
-
+export default {
+  data() {
+    return {
+      cards: [],
+      loadingCards: false,
+      setCodes: [],
+      setCode: '',
+      page: 1,
+      totalPages: 0,
+    };
+  },
+  methods: {
+    loadCards() {
+      this.loadingCards = true;
+      if (this.setCode !== '') {
+        fetchCardsBySetCode(this.setCode, this.page).then((data) => {
+          this.loadingCards = false;
+          this.cards = data.cards;
+          this.totalPages = data.totalPages;
+        });
+      } else {
+        fetchAllCardsWithPagination(this.page).then((data) => {
+          this.loadingCards = false;
+          this.cards = data.cards;
+          this.totalPages = data.totalPages;
+        });
+      }
+    },
+    clearCodes() {
+      this.setCode = '';
+      this.page = 1;
+    },
+    switchPage(number) {
+      this.page += number;
+      this.verifyPage();
+    },
+    verifyPage() {
+      if (this.page < 1) {
+        this.page = 1;
+      } else if (this.page > this.totalPages) {
+        this.page = this.totalPages;
+      }
+    },
+  },
+  created() {
+    this.loadCards();
+    fetchSetCode().then((setCodes) => {
+      this.setCodes = setCodes;
+    });
+  },
+  watch: {
+    page() {
+      this.loadCards();
+    },
+    setCode() {
+      this.page = 1;
+      this.loadCards();
+    },
+  },
+};
 </script>
 
 <template>
+  <div>
+    <h1>Toutes les cartes</h1>
+  </div>
+  <div class="flex-row">
     <div>
-        <h1>Toutes les cartes</h1>
+      Filtrer par :
+      <select v-model="setCode">
+        <option value="" selected disabled hidden>Choisir un setCode</option>
+        <option v-for="setCode in setCodes" :key="setCode" :value="setCode">
+          {{ setCode }}
+        </option>
+      </select>
+      <button v-if="setCode" @click="clearCodes">Supprimer le filtre</button>
     </div>
-    <div class="card-list">
-        <div v-if="loadingCards">Loading...</div>
-        <div v-else>
-            <div class="card-result" v-for="card in cards" :key="card.id">
-                <router-link :to="{ name: 'get-card', params: { uuid: card.uuid } }">
-                    {{ card.name }} <span>({{ card.uuid }})</span>
-                </router-link>
-            </div>
-        </div>
+    <div>
+      Pagination :
+      <button @click="switchPage(-10)"><<<</button>
+      <button @click="switchPage(-1)"><</button>
+      <span>{{ page }}</span>
+      <button @click="switchPage(1)">></button>
+      <button @click="switchPage(10)">>>></button>
     </div>
+  </div>
+  <div class="card-list">
+    <div v-if="loadingCards">Loading...</div>
+    <div v-else>
+      <div class="card-result" v-for="card in cards" :key="card.id">
+        <router-link :to="{ name: 'get-card', params: { uuid: card.uuid } }">
+          {{ card.name }} <span>({{ card.uuid }})</span>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
+
+
--- assets/pages/SearchPage.vue
+++ assets/pages/SearchPage.vue
@@ -1,17 +1,83 @@
-<script setup>
-// TODO: La page de recheche de cartes.
+<script>
+import { fetchCardsByName, fetchSetCode } from '../services/cardService.js';
+
+export default {
+  data() {
+    return {
+      cards: [],
+      displayCards: [],
+      loadingCards: false,
+      search: '',
+      setCodes: [],
+      setCode: '',
+    };
+  },
+  methods: {
+    searchCards() {
+      if (this.search.length < 3) {
+        this.loadingCards = false;
+        return;
+      }
+      this.loadingCards = true;
+      fetchCardsByName(this.search).then((cards) => {
+        this.loadingCards = false;
+        if (this.setCode !== '') {
+          this.filterCards();
+        }
+        this.cards = cards;
+        this.displayCards = cards;
+      });
+    },
+    clearSearch() {
+      this.search = '';
+      this.cards = [];
+    },
+    clearCodes() {
+      this.setCode = '';
+      this.displayCards = this.cards;
+    },
+    filterCards() {
+      this.displayCards = this.cards.filter((card) => card.setCode === this.setCode);
+    },
+  },
+  created() {
+    fetchSetCode().then((setCodes) => {
+      this.setCodes = setCodes;
+    });
+  },
+
+};
 </script>
 
 <template>
+  <div>
+    <h1>Rechercher une Carte</h1>
+  </div>
+  <div class="flex-row">
     <div>
-        <h1>Rechercher une Carte</h1>
+      Filtrer par :
+      <select v-model="setCode">
+        <option value="" selected disabled hidden>Choisir un setCode</option>
+        <option @click="filterCards" v-for="setCode in setCodes" :key="setCode" :value="setCode">
+          {{ setCode }}
+        </option>
+      </select>
+      <button v-if="setCode" @click="clearCodes">Supprimer le filtre</button>
     </div>
-    <div class="card-list">
-        <div v-if="loadingCards">Loading...</div>
-        <div v-else>
-            <div class="card" v-for="card in cards" :key="card.id">
-                <router-link :to="{ name: 'get-card', params: { uuid: card.uuid } }"> {{ card.name }} - {{ card.uuid }} </router-link>
-            </div>
-        </div>
+    <div>
+      <input type="text" v-model="search" @input="searchCards" />
+      <button type="reset" @click="clearSearch">Vider</button>
     </div>
+  </div>
+  <div class="card-list">
+    <div v-if="loadingCards">Loading...</div>
+    <div v-else>
+      <div class="card" v-for="card in displayCards" :key="card.id">
+        <router-link :to="{ name: 'get-card', params: { uuid: card.uuid } }"> {{ card.name }} - {{
+            card.uuid
+          }}
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
